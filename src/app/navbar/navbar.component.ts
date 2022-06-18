a0915bbe26259058407263ca62cc4eb42f179a40
ssstefannn <steffpotter@gmail.com>
2022-06-18 19:13:25 +0200

    played with fetch function

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,6 +3,10 @@ import { cilHamburgerMenu,cilApplications,cilBell,cilMediaPlay,cilMagnifyingGlas
 import { IconModule, IconSetModule, IconSetService } from '@coreui/icons-angular';
 import { HamburgerService } from '../hamburger.service';
 import { Subscription } from 'rxjs';
+//import whats necessary for the fetch function
+import { HttpClient } from '@angular/common/http';
+
+
 
 @Component({
   selector: 'app-navbar',
@@ -31,6 +35,24 @@ export class NavbarComponent implements OnInit {
 
   hamburgerClickedEvent(){
     this.hamburgerService.changeFlag(!this.hamburgerClicked);
+  }
+
+  async searchClickedEvent(){
+    var data : void | Blob;
+    //make a random number from 100 to 1000
+    var randomNumber1 = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+    var randomNumber2 = Math.floor(Math.random() * (1000 - 100 + 1)) + 100;
+    data = await fetch('https://placekitten.com/'+randomNumber1+'/'+randomNumber2).then(response => 
+      response.blob()
+    )
+    .then(blob => {
+      const urlCreator = window.URL || window.webkitURL;
+      var img = document.createElement('img');
+      img.src = urlCreator.createObjectURL(blob);
+      document.getElementById('pfp')?.setAttribute('src',img.src);
+    });
+    
+    
   }
 
 
